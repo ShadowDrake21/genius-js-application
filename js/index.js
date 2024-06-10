7757a0d755654728f67a501f481af6eff5b1793d
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-10 16:01:50 +0300

    get-lyrics: part 2

--- js/index.js
+++ js/index.js
@@ -1,26 +1,5 @@
 import { CLIENT_ACCESS_TOKEN } from '../environment.js';
+import { getSong, search } from './api.js';
 
-async function getLyrics(artist) {
-  // const searchUrl = `http://api.genius.com/search?q=${artist}&access_token=${CLIENT_ACCESS_TOKEN}`;
-  const searchUrl = `http://api.genius.com/songs/${artist}?access_token=${CLIENT_ACCESS_TOKEN}`;
-
-  try {
-    fetch(searchUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  } catch (err) {
-    console.log(err.toJSON());
-  }
-}
-
-const init = (artist) => {
-  getLyrics(artist).then((value) => {
-    console.log(value);
-  });
-};
-
-init('7022998');
+search('ostatnia umiera nadzieja');
+getSong(7904735);
